4b42cd9ab3d8a6beffac35781bb4e2e8ed4e2752
root <root@test-RR556AA-AB2-a1520cn.(none)>
2015-02-26 23:38:17 +0800

    first commit

---
+++ test/test-iterator.py
@@ -0,0 +1,59 @@
+import leveldb
+db = leveldb.LevelDB("/tmp/test-leveldb/")
+
+batch = leveldb.WriteBatch();
+
+for i in xrange(1000):
+	batch.Put(str(i), "hello, hello, hello string %s" %i)
+
+print "Print a string before WriteBatch takes effect. "
+print db.Get("888")
+print "\n"
+
+
+db.Write(batch)
+
+print "Print a string after WriteBatch takes effect. "
+print db.Get("888")
+
+
+print "Creating iterator"
+
+iter = leveldb.Iterator(db);
+print "Seek to First.------------------>"
+s=iter.First()
+print 'iter.First:',s
+
+print 'k,v=',iter.Key(),iter.Value()
+
+
+print "\n"
+
+print "Print iterator's value"
+print iter.Value()
+print "\n"
+
+print "Seek to next"
+iter.Next()
+print "\n"
+
+print "Print iterator's key again"
+print iter.Key()
+print "\n"
+
+print "Print iterator's value again"
+print iter.Value()
+print "\n"
+
+print "Seek to Last"
+iter.Last()
+print "\n"
+
+print "Print iterator's key again"
+print iter.Key()
+print "\n"
+
+print "Print iterator's value again"
+print iter.Value()
+print "\n"
+
